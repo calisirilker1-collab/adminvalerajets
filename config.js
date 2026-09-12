36b54543a3d8449357ab1f5310dfe8d60110c2b3
calisirilker1-collab <calisirilker1@gmail.com>
2026-09-12 17:46:48 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Public browser configuration. This publishable key is safe to use in a browser.
-// Never put a Supabase secret/service_role key in this file.
-window.VALERA_ADMIN_CONFIG = {
-  supabaseUrl: 'https://iczidgdfpirtltopvddx.supabase.co',
-  publishableKey: 'sb_publishable_nwUAvBU5k4NS8hB9cmNLHg__I_uNnMk'
-};
